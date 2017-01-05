ef75a701dfa497f46bd9af60c0ced8c9807c0a20
illescasDaniel <illescas.daniel@protonmail.com>
2017-01-05 13:21:35 +0100

    Initial Commit

---
+++ Array/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Array
+//
+//  Created by Daniel Illescas Romero on 05/01/2017.
+//  Copyright © 2017 Daniel Illescas Romero. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
